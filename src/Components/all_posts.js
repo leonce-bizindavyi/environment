a9654a15eb78c3c16977449f96082939703bfcd3
leonce-bizindavyi <leonceb900@gmail.com>
2023-09-23 10:15:54 +0200

    first commit

--- src/Components/all_posts.js
+++ src/Components/all_posts.js
@@ -161,7 +161,7 @@ const afficher_autres_commentaires = ()=>{
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-gray-600 group-hover:text-emerald-900">
                                   <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                                 </svg>
-                                <span className="absolute -mt-12  -ml-5 text-emerald-900 invisible group-hover:visible">j'aime</span>
+                                <span className="absolute -mt-12  -ml-5 text-emerald-900 invisible group-hover:visible">j&apos;aime</span>
                               </button>
                               <button onClick={afficher_ecrire} className='relative group'>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 text-gray-600 group-hover:text-emerald-900">
@@ -261,7 +261,7 @@ const afficher_autres_commentaires = ()=>{
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-gray-600 group-hover:text-emerald-900">
                                   <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                                 </svg>
-                                <span className="absolute -mt-12  -ml-5 text-emerald-900 invisible group-hover:visible">j'aime</span>
+                                <span className="absolute -mt-12  -ml-5 text-emerald-900 invisible group-hover:visible">j&apos;aime</span>
                               </button>
                               <button onClick={afficher_ecrire} className='relative group'>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 text-gray-600 group-hover:text-emerald-900">
@@ -361,7 +361,7 @@ const afficher_autres_commentaires = ()=>{
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-gray-600 group-hover:text-emerald-900">
                                   <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                                 </svg>
-                                <span className="absolute -mt-12  -ml-5 text-emerald-900 invisible group-hover:visible">j'aime</span>
+                                <span className="absolute -mt-12  -ml-5 text-emerald-900 invisible group-hover:visible">j&apos;aime</span>
                               </button>
                               <button onClick={afficher_ecrire} className='relative group'>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 text-gray-600 group-hover:text-emerald-900">
